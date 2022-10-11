fef8c2739aa4d46fd803c22d037fe2f31b6156ab
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-10-11 16:20:36 -0400

    starting the function to change the the value of the image in the main menu

--- app/src/main/java/dawsoncollege/android/food_ilicious/MainActivity.kt
+++ app/src/main/java/dawsoncollege/android/food_ilicious/MainActivity.kt
@@ -13,4 +13,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
+
+    private fun randFood() {
+        //getting the random values
+        val food_names = resources.getStringArray(R.array.starting_food_names)
+        val food_name = food_names.random()
+        val food_txt = binding.foodTxt
+        val food_img = binding.foodImg
+
+        //setting the random value to be set in screen
+        //setting the value text
+        food_txt.text = food_name
+        //setting the value image
+        val uri = "@drawable/${food_name.lowercase()}"
+        val imageResource = resources.getIdentifier(uri, null, packageName)
+        val res = resources.
+    }
 }
