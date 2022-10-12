8cf5c62975bce20f83dccf5288ee3f38d511a19c
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-10-11 20:56:17 -0400

    made the if statement if a food does not have drawable image, also made the mor info button work as well as the image click function

--- app/src/main/java/dawsoncollege/android/food_ilicious/MainActivity.kt
+++ app/src/main/java/dawsoncollege/android/food_ilicious/MainActivity.kt
@@ -1,7 +1,10 @@
 package dawsoncollege.android.food_ilicious
 
 import android.annotation.SuppressLint
+import android.app.SearchManager
+import android.content.Intent
 import android.content.res.Resources
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
@@ -11,6 +14,12 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    companion object {
+        private const val LOG_TAG = "MAIN_ACTIVITY_DEV_LOG"
+        private const val RN_CHOOSER_REQUEST_CODE = 0
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -18,6 +27,18 @@ class MainActivity : AppCompatActivity() {
         //function to do a random food in a list upon loading and the roll button
         randFood()
         reRollBtn()
+
+        //map nearby for foo text
+        val foodImg = binding.foodImg
+        foodImg.setOnClickListener {
+            mapSearch()
+        }
+
+        //more info button
+        val moreInfoBtn = binding.moreInfoBtn
+        moreInfoBtn.setOnClickListener {
+            webSearch()
+        }
     }
 
 
@@ -38,10 +59,31 @@ class MainActivity : AppCompatActivity() {
         //setting the value text
         foodTxt.text = foodName
         //setting the value image
+        var imageToPut = ""
         val uri = "@drawable/${foodName.lowercase()}"
         val imageResource = resources.getIdentifier(uri, "drawable", packageName)
         foodImg.setImageResource(imageResource)
+        imageToPut = if (foodImg.drawable == null) {
+            "@drawable/default_food"
+        } else {
+            "@drawable/${foodName.lowercase()}"
+        }
+        val imgResource = resources.getIdentifier(imageToPut, "drawable", packageName)
+        foodImg.setImageResource(imgResource)
     }
 
-
+    private fun mapSearch() {
+        val foodTxt = binding.foodTxt.text
+        val intentUri = Uri.parse("geo: 0, 0?q=${foodTxt}")
+        val mapIntent = Intent(Intent.ACTION_VIEW, intentUri)
+        mapIntent.setPackage("com.google.android.apps.maps")
+        startActivity(mapIntent)
+    }
+    private fun webSearch() {
+        val foodTxt = binding.foodTxt.text
+        val searchPrefix = "https://www.google.com/search?q="
+        val queryUrl: Uri = Uri.parse("${searchPrefix}${foodTxt}")
+        val intent = Intent(Intent.ACTION_VIEW, queryUrl)
+        startActivity(intent)
+    }
 }
